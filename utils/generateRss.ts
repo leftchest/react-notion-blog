77789f288a2832a7d16b9864b397f3b520b196a7
TZH001 <22451688@qq.com>
2021-05-31 10:22:41 +0800

    mix

--- utils/generateRss.ts
+++ utils/generateRss.ts
@@ -2,21 +2,21 @@ import { Feed } from 'feed'
 import { Post } from '../pages/index'
 import { formatSlug } from './slugFormat'
 
-const domain = 'https://blog.spencerwoo.com'
+const domain = 'https://blog.hncaa.cn'
 
 export const generateRss = (posts: Post[]) => {
   const year = new Date().getFullYear()
   const feed = new Feed({
     id: domain,
     link: domain,
-    title: "Spencer's Blog",
-    copyright: `All rights reserved ${year}, Spencer Woo`,
+    title: "Leftchest's Blog",
+    copyright: `All rights reserved ${year}, Leftchest`,
     image: `${domain}/favicon.png`,
     favicon: `${domain}/favicon.ico`,
     author: {
-      name: 'Spencer Woo',
-      email: 'spencer.wushangbo@gmail.com',
-      link: 'https://spencerwoo.com'
+      name: 'Leftchest',
+      email: 'leftchest@foxmail.com',
+      link: 'https://hncaa.cn'
     }
   })
 
